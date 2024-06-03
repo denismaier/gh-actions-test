131805b25496450446cb6586a88e31dbf0361b5a
Denis Maier <denis.maier@unibe.ch>
2024-06-03 11:54:54 +0200

    bump version

---
+++ make-pr.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+################################################
+# Get next version number
+################################################
+
+current=$(git describe --tags)
+echo "Current version: $current"
+read -r -p "Enter new version number: " version
+echo "Next version set to: $version"
+
+################################################
+## Update install.rdf and manifest.json
+################################################
+
+perl -pi -e "s/em:version=\"[^\"]*/em:version=\"$version/;" src/install.rdf
+# perl -pi -e "s/\"version\": \"[^\"]*\"/\"version\": \"$version\"/" src/manifest.json
+
+################################################
+## Make PR using the Github CLI
+################################################
+
+git commit -am "bump version"
+gh pr create
